5b3b9fce53885e7d8e115d4b72560248d57a0285
tim <timsregisters@gmail.com>
2023-05-10 19:32:01 +0200

    fix secret_key

--- app/settings.py
+++ app/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('DEBUG', default=False, cast=bool)
+SECRET_KEY = config('SECRET_KEY', default=False, cast=str)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
