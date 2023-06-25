e8a3d389cc179e89d0e2b4ad422d5e11e7f5b084
tim <timsregisters@gmail.com>
2023-06-25 17:11:53 +0200

    refactor

--- goods/repository.py
+++ goods/repository.py
@@ -1,10 +1,10 @@
 from django.db.models import QuerySet, Q
 from graphql import GraphQLResolveInfo
 
-from app.errors import UnauthorizedError
-from app.repository_base import RepositoryBase, IRepository
 from category.models import Category
 from goods.models import Good
+from app.errors import UnauthorizedError, ResourceError
+from app.repository_base import RepositoryBase, IRepository
 from users.models import ExtendedUser
 
 
@@ -13,7 +13,7 @@ class GoodRepository(RepositoryBase, IRepository):
     model = Good
 
     @staticmethod
-    def create_item(info: GraphQLResolveInfo, **kwargs) -> [QuerySet]:
+    def create_item(info: GraphQLResolveInfo = None, **kwargs) -> [QuerySet]:
         user: ExtendedUser = info.context.user or None
         if user.is_seller() or user.is_admin():
             return super(GoodRepository, GoodRepository). \
@@ -24,9 +24,8 @@ class GoodRepository(RepositoryBase, IRepository):
             raise UnauthorizedError(
                 "Not enough permissions to call this endpoint")
 
-
     @staticmethod
-    def get_all_items() -> [QuerySet]:
+    def get_all_items(info: GraphQLResolveInfo = None) -> [QuerySet]:
         return super(GoodRepository, GoodRepository). \
             get_all_items_base(GoodRepository)
 
@@ -37,17 +36,20 @@ class GoodRepository(RepositoryBase, IRepository):
             get_items_by_filter_base(GoodRepository, search_filter)
 
     @staticmethod
-    def get_by_id(searched_id: str) -> [QuerySet]:
+    def get_by_id(searched_id: str,
+                  info: GraphQLResolveInfo = None) -> [QuerySet]:
         return super(GoodRepository, GoodRepository).\
             get_by_id_base(GoodRepository, searched_id)
 
     @staticmethod
-    def update_item(info: GraphQLResolveInfo, item_id, **kwargs) -> [QuerySet]:
+    def update_item(info: GraphQLResolveInfo = None,
+                    **kwargs) -> [QuerySet]:
         user: ExtendedUser = info.context.user or None
-        good = Good.objects.filter(id=item_id).first()
+        good = Good.objects.filter(id=kwargs["item_id"]).first()
         if user.is_admin() or user == good.seller:
             return super(GoodRepository, GoodRepository).\
-                update_item_base(GoodRepository, item_id=item_id, **kwargs)
+                update_item_base(GoodRepository,
+                                 **kwargs)
         else:
             raise UnauthorizedError(
                 "Not enough permissions to call this endpoint")
@@ -56,6 +58,8 @@ class GoodRepository(RepositoryBase, IRepository):
     def delete_item(info: GraphQLResolveInfo, searched_id: str):
         user: ExtendedUser = info.context.user or None
         good = Good.objects.filter(id=searched_id).first()
+        if good is None:
+            raise ResourceError('object with searched id does not exist')
         if user.is_admin() or user.id == good.seller_id:
             super(GoodRepository, GoodRepository). \
                 delete_item_base(GoodRepository, searched_id)
@@ -65,7 +69,11 @@ class GoodRepository(RepositoryBase, IRepository):
                         searched_id: str,
                         category_id: str) -> [QuerySet]:
         category = Category.objects.filter(id=category_id).first()
+        if category is None:
+            raise ResourceError('object with searched id does not exist')
         good = Good.objects.filter(id=searched_id).first()
+        if good is None:
+            raise ResourceError('object with searched id does not exist')
         user: ExtendedUser = info.context.user or None
         if user.is_admin() or user == good.seller:
             good.category = category
